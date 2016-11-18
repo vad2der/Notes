1ccb3ab49a46f3af55e1769fe2135e94b5bac3a4
VadimDeryabin <vad.deryabin@gmail.com>
2016-11-18 14:13:11 -0700

    Update console.rb

--- console.rb
+++ console.rb
@@ -3,3 +3,6 @@ rails console
 
 # to list all classes in models, even if they are not using ORMs
 Dir['**/models/**/*.rb'].map {|f| File.basename(f, '.*').camelize.constantize }
+
+# run server in current folder
+ruby -run -e httpd . -p 9090
