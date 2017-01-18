1c6e3fb9e14916d2b7befb3999bc07314382785b
VadimDeryabin <vad.deryabin@gmail.com>
2017-01-18 08:53:52 -0700

    Update console.py

--- console.py
+++ console.py
@@ -1,2 +1,5 @@
 # run server in current folder
 python3 -m http.server 80
+
+# create virtual environment with particular python version
+virtualenv --python=[path_to_pthon.exe] [path_to_venv_folder]
