a3f23f827a577e0c4f73d871123cf48ba3d23d2d
VadimDeryabin <vad.deryabin@gmail.com>
2017-06-21 16:14:56 -0600

    Update console.py

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
-# run server in current folder
-python3 -m http.server 80
+# run server in current folder for Python 3
+python3 -m http.server 3000
+
+#  run server in current folder for Python 2.7
+python3 -m SimpleHTTPServer 3000
 
 # create virtual environment with particular python version
 virtualenv --python=[path_to_pthon.exe] [path_to_venv_folder]
